aaaab69b6fdc76f504a54fe89a63ad463f294eba
Tanveer Abbas <tanveerabbas20175@gmail.com>
2022-07-15 00:29:11 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TodoComponent } from './todo/todo.component';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { ToastrModule } from 'ngx-toastr';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule,
+    MatCheckboxModule,
+    ToastrModule.forRoot({
+      closeButton: true,
+      progressBar:true,
+      progressAnimation:'increasing',
+
+
+    }),
+    HttpClientModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree,Router } from '@angular/router';
+import { Observable ,map ,tap } from 'rxjs';
+import { ApiService } from '../services/api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+   constructor( private apiService:  ApiService,
+       private router: Router
+    ){
+
+   }
+
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.apiService.jwtUserToken.pipe(
+
+      map(   (result)=> !!result  ),
+      tap(  result => {
+
+
+        if(!result){
+          this.router.navigateByUrl('/login').then();
+          return result;
+        }
+        return result;
+      })
+
+
+
+    );
+
+  }
+  
+}
